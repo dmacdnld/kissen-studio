25153fb309149a9ad30f9896908f685826b42dda
Daniel MacDonald <dnmacdnld@gmail.com>
2014-03-22 15:47:30 -0500

    Refactoring

--- kissen-studio-4559199/assets/shop.js
+++ kissen-studio-4559199/assets/shop.js
@@ -1,81 +1,114 @@
 (function ($, _, undefined) {
+    "use strict";
+
     var shop = {
-        ui: {
-            mainHeader: "#main-header",
-            menuTrigger: "#menu-trigger",
-            mainNav: "#main-nav",
-            productPrice: "#product-price",
-            variantSelect: "#variant-select",
-            quantitySelect: "#quantity-select",
-            productImageListItems: ".product-image-list__item",
-            productThumbnailList: "#product-thumbnail-list"
-        },
+            ui: {
+                mainHeader: "#main-header",
+                menuTrigger: "#menu-trigger",
+                mainMenu: "#main-menu",
+                expandableSearchForms: ".expandable-search-form",
+                productPrice: "#product-price",
+                variantSelect: "#variant-select",
+                quantitySelect: "#quantity-select",
+                productImageListItems: ".product-image-list__item",
+                productThumbnailList: "#product-thumbnail-list"
+            },
 
-        toggleMenu: function (evt) {
-            var menuShouldDisplay;
+            toggleMenu: function (evt) {
+                if (!_.contains(whitelistedKeyCodes, evt.keyCode) && evt.type !== "click") return;
 
-            if (evt.keyCode === 13 || evt.keyCode === 32) {
-                evt.preventDefault();
-            }
-            else if (evt.keyCode === 27) {
-                menuShouldDisplay = false;
-            }
+                var menuShouldDisplay;
 
-            $(shop.ui.mainNav).toggleClass("display--block", menuShouldDisplay);
-            $(shop.ui.menuTrigger).toggleClass("link--current");
-        },
-
-        getQuantityOptions: function () {
-            return _.map(productData.variants, function (variant) {
-                var i = 1,
-                    length = variant.inventory_quantity,
-                    html = [],
-                    $clonedQuantitySelect = $(shop.ui.quantitySelect).clone();
-
-                while (i <= length) {
-                    html.push("<option value=\"" + i + "\">" + i + "</option>");
-                    i++;
+                if (evt.keyCode === 13 || evt.keyCode === 32) {
+                    evt.preventDefault();
+                }
+                else if (evt.keyCode === 27) {
+                    menuShouldDisplay = false;
                 }
 
-                return {
-                    "variantId": variant.id,
-                    "$select": $clonedQuantitySelect.html(html.join(" "))
-                };
-            });
-        },
+                $(shop.ui.menuTrigger).toggleClass("button--positive");
+                $(shop.ui.mainMenu).toggleClass("display--inline-block", menuShouldDisplay);
+            },
 
-        swapQuantityOptions: function (evt) {
-            var selectedVariant = evt.currentTarget.selectedOptions[0],
-                variantQuantityOptions = _.find(quantityOptions, function (option) {
-                    return option.variantId === parseInt(selectedVariant.value, 10);
+            toggleSearchForm: function (evt) {
+                var $form = $(this),
+                    expandedClass = "expandable-search-form--expanded",
+                    formIsExpanded = $form.hasClass(expandedClass),
+                    $input = $form.children("input");
+
+                if (evt && !$input.val()) {
+                    evt.preventDefault();
+                }
+                else {
+                    return;
+                }
+
+                if (formIsExpanded) {
+                    $form.removeClass(expandedClass);
+                }
+                else {
+                    $form.addClass(expandedClass);
+                    $form.children(".search__input").focus();
+                }
+            },
+
+            getQuantityOptions: function () {
+                return _.map(productData.variants, function (variant) {
+                    var i = 1,
+                        length = variant.inventory_quantity,
+                        html = [],
+                        $clonedQuantitySelect = $(shop.ui.quantitySelect).clone();
+
+                    while (i <= length) {
+                        html.push("<option value=\"" + i + "\">" + i + "</option>");
+                        i++;
+                    }
+
+                    return {
+                        "variantId": variant.id,
+                        "$select": $clonedQuantitySelect.html(html.join(" "))
+                    };
                 });
+            },
 
-            $(shop.ui.quantitySelect).replaceWith(variantQuantityOptions.$select);
-            $(shop.ui.productPrice).text(selectedVariant.dataset.price);
+            swapQuantityOptions: function (evt) {
+                var selectedVariant = evt.currentTarget.selectedOptions[0],
+                    variantQuantityOptions = _.find(quantityOptions, function (option) {
+                        return option.variantId === parseInt(selectedVariant.value, 10);
+                    });
+
+                $(shop.ui.quantitySelect).replaceWith(variantQuantityOptions.$select);
+                $(shop.ui.productPrice).text(selectedVariant.dataset.price);
+            },
+
+            swapProductImages: function (evt) {
+                var imageId = evt.currentTarget.id.split("-")[2];
+
+                $(shop.ui.productImageListItems).addClass("hidden");
+
+                $("#product-image-" + imageId)
+                    .parent()
+                    .removeClass("hidden");
+            }
         },
-
-        swapProductImages: function (evt) {
-            var imageId = evt.currentTarget.id.split("-")[2];
-
-            $(shop.ui.productImageListItems).addClass("hidden");
-
-            $("#product-image-" + imageId)
-                .parent()
-                .removeClass("hidden");
-        }
-    };
+        whitelistedKeyCodes = [
+            13, // enter
+            27, // escape
+            32  // space
+        ];
 
     $(shop.ui.menuTrigger)
         .replaceWith(
-            "<button id=\"menu-trigger\" class=\"menu-trigger\" type=\"button\">" +
+            "<button id=\"menu-trigger\" class=\"menu-trigger\" type=\"button\" tabindex=\"1\">" +
                 "<span class=\"accessibility\">Menu</span>" +
                 "<span id=\"menu-trigger__icon\" aria-hidden=\"true\"></span>" +
             "</button>"
         );
 
-    $(shop.ui.menuTrigger)
-        .on("click", shop.toggleMenu)
-        .on("keydown", shop.toggleMenu);
+    $(shop.ui.menuTrigger).on("click", shop.toggleMenu)
+                          .on("keydown", shop.toggleMenu);
+
+    $(shop.ui.expandableSearchForms).on("submit", shop.toggleSearchForm);
 
     if (_.contains(_.keys(window), "productData")) {
         var quantityOptions = shop.getQuantityOptions();
